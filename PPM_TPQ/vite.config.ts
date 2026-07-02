9d0886b68666eba651b39de57ab16b40fef391b0
Rakha404 <dawgsyogs@gmail.com>
2026-07-02 23:54:00 +0700

    fix: atur base path relatif untuk bundle assets vite

--- PPM_TPQ/vite.config.ts
+++ PPM_TPQ/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 👈 Pastikan plugin ini di-import
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 👈 Jalankan fungsinya di sini
+    tailwindcss(),
   ],
+  base: './', // 👈 TAMBAHKAN BARIS INI! 
 })
